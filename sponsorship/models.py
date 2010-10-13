aa0028a167f76533190d002a8fe730a8e71c1dd9
Thomas Wiedenbein <wiede1t@cmich.edu>
2010-10-13 18:28:49 -0400

    Updated the models for sponsorship

--- sponsorship/models.py
+++ sponsorship/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.localflavor.us.models import PhoneNumberField
 
 class Sponsor(models.Model):
+    # TODO: Add support for Events.
+    
+    def __unicode__(self):
+        return self.name
+        
     STATUS_TYPES = (
         ('CONF', 'Confirmed'),
         ('DENY', 'Denied'),
@@ -9,20 +14,28 @@ class Sponsor(models.Model):
         ('CONT', 'Contacted'),
         ('FOLL', 'Followed-Up'),
         ('NOCO', 'Not Contacted'),
+        ('DEAD', 'Dead Contact'),
     )
-    company_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Sponsor Name")
     contact_name = models.CharField(max_length=50, blank=True)
-    contact_email = models.EmailField()
+    contact_email = models.EmailField(blank=True)
     contact_phone = PhoneNumberField(blank=True)
-    sponsor_status = models.CharField(max_length=4, choices=STATUS_TYPES, default='NOCO')
+    status = models.CharField(max_length=4, choices=STATUS_TYPES, default='NOCO')
     # will have to change this later
-    lan_rep = models.CharField(max_length=20)
-    notes = models.TextField()
+    lan_rep = models.CharField(max_length=20, verbose_name="LAN Representative")
+    notes = models.TextField(blank=True)
+    
 
 class Prize(models.Model):
+    # TODO: Add support for Tournaments.
+    # TODO: Add image support.
+    
+    def __unicode__(self):
+        return self.name
+        
     sponsor = models.ForeignKey(Sponsor)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100, verbose_name="Prize Name")
+    description = models.TextField(blank=True)
 
     
     
