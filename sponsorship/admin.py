87985dfeb12ec4694d7cf224dbeef0ad2c82ade7
Thomas Wiedenbein <wiede1t@cmich.edu>
2010-10-17 17:40:45 -0400

    Added relationship for Sponsors to Event, added validation to relationship between Prize and Event

--- sponsorship/admin.py
+++ sponsorship/admin.py
@@ -3,6 +3,7 @@ from sponsorship.models import Prize
 from django.contrib import admin
 from django.contrib.auth.models import User 
 from django import forms
+import datetime
 
 class PrizeAdminForm(forms.ModelForm):
     class Meta:
@@ -12,17 +13,22 @@ class PrizeAdminForm(forms.ModelForm):
         if self.cleaned_data['sponsor'].status != 'c':
             raise forms.ValidationError("Sponsor must be confirmed before prizes can be added.")
         return self.cleaned_data['sponsor']
-
+    
+    def clean_event(self):
+        if self.cleaned_data['event'].end_date < datetime.datetime.now():
+            raise forms.ValidationError("Prizes cannot be added to events in the past.")
+        return self.cleaned_data['event']
+        
 class SponsorAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Contact Information', {'fields': ['name', 'contact_name',
             'contact_email', 'contact_phone', 'status']}),
             
-        ('Other Information',   { 'fields': ['lan_rep', 'notes']}),
+        ('Other Information',   { 'fields': ['lan_rep', 'notes', 'event']}),
     ]
     
-    list_display = ('name', 'status', 'lan_rep')
-    list_filter = ('status', 'lan_rep',)
+    list_display = ('name', 'status', 'lan_rep',)
+    list_filter = ('status', 'lan_rep', 'event')
     search_fields = ('name',)
     actions = ['set_followup', 'reset_sponsors']
     
@@ -38,14 +44,29 @@ class SponsorAdmin(admin.ModelAdmin):
         if db_field.name == 'lan_rep': 
             kwargs['queryset'] = User.objects.all() 
             kwargs['initial'] = request.user.id 
-            return db_field.formfield(**kwargs) 
+            return db_field.formfield(**kwargs)
         return super(SponsorAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        
+    # def formfield_for_manytomany(self, db_field, request, **kwargs):
+    #      if db_field.name == "event":
+    #          kwargs['queryset'] = Sponsor.objects.filter(event__start_date__gt = datetime.datetime.now())
+    #          kwargs['initial'] = req
+    #          return db_field.formfield(**kwargs)
+    #      return super(SponsorAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
 class PrizeAdmin(admin.ModelAdmin):
     form = PrizeAdminForm
-    fields = ('sponsor', 'name', 'description', 'raffle_prize')
-    list_display = ('name', 'sponsor')
+    fields = ('sponsor', 'name', 'description', 'raffle_prize', 'event')
+    list_display = ('name', 'sponsor', 'event')
+    list_filter = ('event',)
     search_fields = ('name', 'sponsor')
+    
+    # def formfield_for_foreignkey(self, db_field, request, **kwargs): 
+    #     if db_field.name == 'event': 
+    #         kwargs['queryset'] = Event.objects.all()
+    #         kwargs['initial'] = request.user.id 
+    #         return db_field.formfield(**kwargs)
+    #     return super(SponsorAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
     
 admin.site.register(Sponsor, SponsorAdmin)
--- sponsorship/models.py
+++ sponsorship/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.localflavor.us.models import PhoneNumberField
 from django.contrib.auth.models import User
 
+# class SponsorManager(models.Manager):
+#     def get_query_set(self):
+#         return super(SponsorManager, self).get_query_set().annotate(prizecount=models.Count('prize'))
+
 class Sponsor(models.Model):
     # TODO: Add support for Events.
     
@@ -23,9 +27,14 @@ class Sponsor(models.Model):
     contact_email = models.EmailField(blank=True)
     contact_phone = PhoneNumberField(blank=True)
     status = models.CharField(max_length=1, choices=STATUS_TYPES, default='n')
-    # will have to change this later
     lan_rep = models.ForeignKey(User, verbose_name="LAN Representative")
     notes = models.TextField(blank=True)
+    event = models.ManyToManyField('events.Event')
+    # objects = SponsorManager()
+    
+    # def count(self):
+    #     return self.prizecount
+    # count.admin_order_field = 'prizecount'
     
 
 class Prize(models.Model):
@@ -35,10 +44,13 @@ class Prize(models.Model):
     def __unicode__(self):
         return self.name
         
+    event = models.ForeignKey('events.Event')
     sponsor = models.ForeignKey(Sponsor)
     name = models.CharField(max_length=100, verbose_name="Prize Name")
     description = models.TextField(blank=True)
     raffle_prize = models.BooleanField(verbose_name="Raffle Prize?")
+    
+
 
     
     
