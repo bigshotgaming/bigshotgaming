83ff7fc2f491350558d060d825c5be5423ba7556
Bryan Koroleski <korol1bp@cmich.edu>
2010-10-16 23:43:58 -0400

    Installed django-registration.

--- settings.py
+++ settings.py
@@ -79,10 +79,10 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'bigshotgaming.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    'templates/'
 )
+
+ACCOUNT_ACTIVATION_DAYS = '7'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -96,4 +96,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'events',
     'sponsorship',
+    'registration',
+    'south',
 )
