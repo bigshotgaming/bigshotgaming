846054aa3c6d1b5bb8658469e717f673b33786bd
Bryan Koroleski <korol1bp@cmich.edu>
2010-11-04 21:09:23 -0400

    Improved the look of the admin page.

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Section, Post
 
-admin.site.register(Post)
-admin.site.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    filter_horizontal = ('posts',)
+    prepopulated_fields = {'url': ('name',)}
+    
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'is_published')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Section, SectionAdmin)
--- pages/models.py
+++ pages/models.py
@@ -19,7 +19,11 @@ class Section(models.Model):
 class Post(models.Model):
     title = models.CharField(blank=False, null=False, max_length=150)
     content = models.TextField('Message content', help_text='Use Markdown syntax.')
-    pub_date = models.DateTimeField('Date published', default=datetime.datetime.now)
+    pub_date = models.DateTimeField('Date published', null=True, blank=True)
+    
+    def is_published(self):
+        return self.pub_date is not None and self.pub_date <= datetime.datetime.now()
+    is_published.boolean = True
     
     def __unicode__(self):
-        return self.title
+        return u"%s | %s | %s" % (self.title, self.pub_date, self.is_published())
