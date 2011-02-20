4ca1c3780f72c747fead4bfadf315b5e6780b673
Bryan Koroleski <korol1bp@cmich.edu>
2011-02-19 21:20:13 -0500

    Hooked up the news forum to the index page.

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from djangobb_forum.models import Category
 
 def index(request):
-    return render_to_response('index.html', context_instance=RequestContext(request))
+    posts = Category.objects.get(name='News').forums.get(name='News').topics.select_related().order_by('-created')[:3]
+    return render_to_response('index.html', {'news_posts':posts}, context_instance=RequestContext(request))
