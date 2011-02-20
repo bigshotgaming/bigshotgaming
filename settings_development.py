a29cee570cb90f599831a46315845d8e9251ca57
Bryan Koroleski <korol1bp@cmich.edu>
2011-02-19 23:14:40 -0500

    Re-enabled the sites framework.

--- settings_development.py
+++ settings_development.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.admin',
     'django.contrib.admindocs',
