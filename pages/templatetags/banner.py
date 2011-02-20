55c526beef2f3291fc474558718db5c8c9cda198
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-02-20 01:05:17 -0500

    Tom fail number seven.

--- pages/templatetags/banner.py
+++ pages/templatetags/banner.py
@@ -6,4 +6,4 @@ register = template.Library()
 @register.inclusion_tag('pages/banner.html')
 def get_banner():
     sponsors = Sponsor.objects.filter(event=2, eventsponsor__status='c').exclude(banner=None)
-    return {'sponsors': sponsors}
+    return {'sponsors': list(sponsor)}
