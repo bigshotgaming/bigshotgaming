52372a61260add999216d5625aaf06c30d762a4a
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-02-20 00:54:05 -0500

    Tom fail number two.

--- pages/templatetags/banner.py
+++ pages/templatetags/banner.py
@@ -3,7 +3,7 @@ from sponsorship.models import Sponsor, EventSponsor
 
 register = template.Library()
 
-@register_inclusion_tag('/pages/banner.html')
+@register.inclusion_tag('/pages/banner.html')
 def get_banner():
     sponsors = Sponsor.objects.filter(event=2, eventsponsor__status='c').exclude(banner=None)
     return sponsors
