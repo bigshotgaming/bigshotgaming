e546fe65e240f50c0ec9c9c44575f415fb3c0cd8
Bryan Koroleski <korol1bp@cmich.edu>
2011-02-19 21:46:09 -0500

    Need to check to make sure the forum is configured correctly before trying to render the news.

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
-# Create your views here.
 from django.shortcuts import render_to_response
+from django.core.exceptions import ObjectDoesNotExist
 from django.template import RequestContext
 from djangobb_forum.models import Category
 
 def index(request):
-    posts = Category.objects.get(name='News').forums.get(name='News').topics.select_related().order_by('-created')[:3]
+    try:
+        posts = Category.objects.get(name='News').forums.get(name='News').topics.select_related().order_by('-created')[:3]
+    except ObjectDoesNotExist:
+        posts = None
     return render_to_response('index.html', {'news_posts':posts}, context_instance=RequestContext(request))
