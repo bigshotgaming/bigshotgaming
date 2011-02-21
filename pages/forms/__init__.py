4c812968107e6931203f28ae71a5733af02063a7
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-02-21 15:17:50 -0500

    Shuffled things around a little bit with forms in the pages app

--- pages/forms/__init__.py
+++ pages/forms/__init__.py
@@ -1,5 +1,6 @@
 import re
 from django import forms
+from django.contrib.auth.models import User
 from registration.forms import RegistrationFormUniqueEmail
 from pages.forms.recaptcha import ReCaptchaField
 
@@ -13,3 +14,8 @@ class RegistrationForm(RegistrationFormUniqueEmail):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['username'].regex = re.compile(r"^[\w\s-]+$", re.UNICODE)
         self.fields['captcha'] = ReCaptchaField(remote_ip=remote_ip)
+
+class ContactForm(forms.Form):
+    staff_member = forms.ModelChoiceField(queryset=User.objects.filter(is_staff=True))
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.widgets.Textarea)
