13822b6a5e349aadaa02ece1ae12453b74959d8d
Bryan Koroleski <korol1bp@cmich.edu>
2011-02-23 01:00:25 -0500

    Removed forms.py to prevent clobbering issues.

--- pages/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-
-class ContactForm(forms.Form):
-    staff_member = forms.ModelChoiceField(queryset=User.objects.filter(is_staff=True))
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.widgets.Textarea)
