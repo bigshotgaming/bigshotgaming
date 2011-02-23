cfd6e28e971640203098ce0a3331c9977b78f4b9
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-02-23 01:57:00 -0500

    Fixed issue with banner not checking for the right value in the DB

--- pages/templatetags/banner.py
+++ pages/templatetags/banner.py
@@ -5,5 +5,6 @@ register = template.Library()
 
 @register.inclusion_tag('pages/banner.html')
 def get_banner():
-    sponsors = Sponsor.objects.filter(event=2, eventsponsor__status='c').exclude(banner=None)
+    sponsors = Sponsor.objects.filter(event=2, eventsponsor__status='c').exclude(banner='')
+    print sponsors
     return {'sponsor': list(sponsors)}
