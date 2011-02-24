591641c889821a6d6aa7dc8ceab1affe2d82d3d3
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-02-24 07:46:19 -0500

    Added rough version of sponsors page

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('pages.views',
     (r'^reviews', 'reviews', {}, 'pages_reviews'),
     (r'^contact', 'contact', {}, 'pages_contact'),
+    (r'^sponsors', 'sponsors', {}, 'pages_sponsors'),
     (r'^$', 'index', {}, 'pages_index'),
 )
