aaae27f9467d518d127a24750110855246cfbd1f
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-04-15 22:26:31 -0400

    Refactored the automated banner selection code. Made it so that it's dynamic and not locked to event.id = 2

--- pages/templatetags/banner.py
+++ pages/templatetags/banner.py
@@ -1,9 +1,19 @@
+import datetime
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
+from events.models import Event
 from sponsorship.models import Sponsor, EventSponsor
 
 register = template.Library()
 
 @register.inclusion_tag('pages/banner.html')
 def get_banner():
-    sponsors = Sponsor.objects.filter(event=2, eventsponsor__status='c').exclude(banner='')
+    '''
+    blatantly stolen from the sponsors view
+    '''
+    try:
+        event = Event.objects.get(is_active=True)
+    except ObjectDoesNotExist:
+        event = Event.objects.filter(end_date__lte=datetime.datetime.now())
+    sponsors = Sponsor.objects.filter(event=event, eventsponsor__status__in=['p', 'c', 'r', 'f']).exclude(banner='')
     return {'sponsor': list(sponsors)}
