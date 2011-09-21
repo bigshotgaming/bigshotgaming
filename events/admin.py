ef3908808e3bd76091cb5e0334d9e619b2e7be8f
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-09-21 03:55:05 -0400

    Got rid of the stupid admin site addition for pay at the door and changed it to a simple admin action

--- events/admin.py
+++ events/admin.py
@@ -1,63 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render_to_response
 from django.contrib import admin
-from django.conf.urls.defaults import patterns, include, url
-from django import forms
-from django.template import RequestContext
 from events.models import Event, Venue, Participant, Coupon
 
-class ATDForm(forms.Form):
-    participant = forms.ModelChoiceField(queryset=Participant.objects.all(), required=True)
-    
+def mark_as_paid(modeladmin, request, queryset):
+    for participant in queryset:
+        participant.coupon = Coupon()
+        participant.coupon.activate()
+        participant.save()
+mark_as_paid.short_description = "Mark selected participants as paid"
+
 class ParticipantAdmin(admin.ModelAdmin):    
-    def get_urls(self):
-        urls = super(ParticipantAdmin, self).get_urls()
-        admin_urls = patterns('', 
-            url(r'^payatd/$', self.admin_site.admin_view(self.pay_atd)),
-        )
-        
-        return admin_urls + urls
-        
-    def pay_atd(self, request):
-        if request.method == 'POST':
-            form = ATDForm(request.POST)
-            if form.is_valid():
-                coupon = Coupon()
-                coupon.save()
-                participant=form.cleaned_data['participant']
-                participant.coupon = coupon
-                participant.save()
-    
-                #form.cleaned_data['participant'].is_paid = True
-                #form.cleaned_data['participant'].save()
-                
-                print form.cleaned_data
-                #for name in form.cleaned_data['names']:
-                #    import_user.delay(name, category=form.cleaned_data['category'], user=request.user)
-                #self.message_user(request, 'Importing users...')
-                #return redirect('admin:namelist_player_changelist')
-
-        form = ATDForm()
-        context = RequestContext(request, current_app=self.admin_site.name)
-        return render_to_response('events/admin/change_form.html', {
-            'app_label': self.model._meta.app_label,
-            'opts': self.model._meta,
-            'has_add_permission': True,
-            'has_delete_permission': True,
-            'has_change_permission': True,
-            'show_delete':True,
-            'add': True,
-            'change': False,
-            'is_popup': False,
-            'save_as': True,
-            'title': 'At-The-Door Payment',
-            'form': form,
-            'media': self.media,
-        }, context_instance=context)
     readonly_fields = ('signup_time',)
-    list_display = ('user', 'event', 'coupon',)
+    list_display = ('user', 'event', 'coupon', 'signup_time')
+    list_filter = ('event')
+    actions = [mark_as_paid]
     
-
 class EventAdmin(admin.ModelAdmin):
     fields = (
         'name', 'start_date', 'end_date', 'venue',
@@ -74,7 +31,6 @@ class EventAdmin(admin.ModelAdmin):
 
 class CouponAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid', 'transaction', 'created_time')
-    
     list_display = ('uuid', 'transaction', 'activated', 'created_time', 'activated_time')
     
     
