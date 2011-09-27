c55221c7ae705c25ff056e7da9a3d590d16413d1
Thomas Wiedenbein <wiede1t@cmich.edu>
2011-09-27 18:47:25 -0400

    Added more Travis changes

--- seatmap/urls.py
+++ seatmap/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'seatmap.views.seatmap_display'),
-    (r'^seat/(?P<x>\d+)/(?P<y>\d+)/$', 'seatmap.views.seat_display'),
+    (r'^seat/(?P<seat>\d+)/$', 'seatmap.views.seat_display'),
+    (r'^admin/$', 'seatmap.views.seatmap_admin'),
+    (r'^admin/seat/(?P<seat>\d+)/$', 'seatmap.views.seat_admin'),
+    (r'^admin/seat/create/$', 'seatmap.views.seat_create'),
+    (r'^admin/table/create/$', 'seatmap.views.table_create'),
 )
