457befd770415f58a733bdadba1a877ab363105b
Thomas Wiedenbein <wiede1t@cmich.edu>
2012-08-30 01:46:25 -0400

    More DjangoBB cleanup work...I think we're almost done here.

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from djangobb_forum.admin import UserAdmin
 from django import forms
 from pages.models import Post
 
@@ -23,7 +23,7 @@ class ExtendedUserAdmin(UserAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title']
-    
-admin.site.unregister(User)
+
+admin.site.unregister(User)   
 admin.site.register(User, ExtendedUserAdmin)
 admin.site.register(Post, PostAdmin)
