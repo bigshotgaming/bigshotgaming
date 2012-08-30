8e30ab1d274aa960d9a35476e928eee6a81f000c
Thomas Wiedenbein <wiede1t@cmich.edu>
2012-08-29 23:27:29 -0400

    Changed the news posting/commenting system to something not DjangoBB. Simple.

---
+++ pages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User 
+
+class Post(models.Model):
+
+    def __unicode__(self):
+        return self.title
+
+    title = models.CharField(max_length=80)
+    body = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("auth.User", limit_choices_to={'is_staff': True})
--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('pages.views',
     (r'^contact', 'contact', {}, 'pages_contact'),
     (r'^sponsors', 'sponsors', {}, 'pages_sponsors'),
     (r'^$', 'index', {}, 'pages_index'),
+    (r"^(\d+)/$", "post"),
 )
