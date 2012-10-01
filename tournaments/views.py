b89bb37abdfcce34a86b1f2ca2d9b16b5ff46779
Thomas Wiedenbein <wiede1t@cmich.edu>
2012-09-30 23:47:50 -0400

    Some ugly hacks to allow non-participants to view the tournament listings.

--- tournaments/views.py
+++ tournaments/views.py
@@ -23,62 +23,74 @@ def index(request):
         'tournaments_list': tournaments_list,
     })
 
-@login_required
 def tournament(request, pk):
     tournament = Tournament.objects.get(pk=int(pk))
     teams = tournament.team_set.all()
     event = tournament.event
-    participant = Participant.objects.get(user=request.user, event=event)
-
-    if request.method == 'POST':
-        form = CreateTeamForm(request.POST, tournament=tournament)
-        if form.is_valid():
-            try:
-                ct = Team.objects.get(members=participant, tournament=tournament)
-                ct.members.remove(participant)
-            except:
-                pass
-            team = Team.objects.create(name=form.cleaned_data['name'], password=form.cleaned_data['password'],
-                tournament=tournament)
-            team.members.add(participant)
-            return HttpResponseRedirect(reverse('tournaments_tournament', args=[tournament.id]))
-    else:
-        form = CreateTeamForm(tournament=tournament)
+    # ugly temporary hacks here
+    form = None
+    participant = None
+    try:
+        participant = Participant.objects.get(user=request.user, event=event)
+        if request.method == 'POST':
+            form = CreateTeamForm(request.POST, tournament=tournament)
+            if form.is_valid():
+                try:
+                    ct = Team.objects.get(members=participant, tournament=tournament)
+                    ct.members.remove(participant)
+                except:
+                    pass
+                team = Team.objects.create(name=form.cleaned_data['name'], password=form.cleaned_data['password'],
+                    tournament=tournament)
+                team.members.add(participant)
+                return HttpResponseRedirect(reverse('tournaments_tournament', args=[tournament.id]))
+        else:
+            form = CreateTeamForm(tournament=tournament)
+    except (TypeError, ObjectDoesNotExist):
+        pass
 
     return render(request, 'tournaments/tournament.html', {
         'event': event,
         'tournament': tournament,
         'teams': teams,
         'form': form,
+        'participant': participant,
     })
 
-@login_required
 def team(request, pk):
 
     team = Team.objects.get(pk=int(pk))
     tournament = team.tournament
     event = tournament.event
-    participant = Participant.objects.get(user=request.user, event=event)
 
+    #more ugly hacks here
+    form = None
+    participant = None
     on_team = False
+    try:
+        participant = Participant.objects.get(user=request.user, event=event)
 
-    if participant in team.members.all():
-        on_team = True
 
-    # thanks to pwf for the following
-    if request.method == 'POST':
-        if not on_team:
-            form = JoinTeamForm(request.POST, team=team, tournament=tournament)
-            if form.is_valid():
-                team.members.add(participant) 
-                return HttpResponseRedirect(reverse('tournaments_team', args=[team.id]))   
+
+        if participant in team.members.all():
+            on_team = True
+
+        # thanks to pwf for the following
+        if request.method == 'POST':
+            if not on_team:
+                form = JoinTeamForm(request.POST, team=team, tournament=tournament)
+                if form.is_valid():
+                    team.members.add(participant) 
+                    return HttpResponseRedirect(reverse('tournaments_team', args=[team.id]))   
+            else:
+                form = LeaveTeamForm(request.POST)
+                if form.is_valid():
+                    team.members.remove(participant)
+                    return HttpResponseRedirect(reverse('tournaments_team', args=[team.id]))   
         else:
-            form = LeaveTeamForm(request.POST)
-            if form.is_valid():
-                team.members.remove(participant)
-                return HttpResponseRedirect(reverse('tournaments_team', args=[team.id]))   
-    else:
-        form = JoinTeamForm(team=team, tournament=tournament)
+            form = JoinTeamForm(team=team, tournament=tournament)
+    except (TypeError, ObjectDoesNotExist):
+        pass
 
     return render(request, 'tournaments/team.html', {
         'event': event,
