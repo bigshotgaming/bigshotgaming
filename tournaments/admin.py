0fbbe6f1729aee4fec6df68f5cbd47d0c5f8f57c
Thomas Wiedenbein <wiede1t@cmich.edu>
2012-10-02 23:34:21 -0400

    Added code to support challonge, including tournament model modifications.

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,14 +1,39 @@
+import challonge
 from django.contrib import admin
-from tournaments.models import Game, Tournament, Team
+from django.template.defaultfilters import slugify
+from tournaments.models import Game, Tournament, Team, _challonge_auth
 
 class GameAdmin(admin.ModelAdmin):
 	pass
 
 class TournamentAdmin(admin.ModelAdmin):
-	pass
+    actions = ['publish_and_start_tournament', 'add_to_challonge']
+
+    def publish_and_start_tournament(self, request, queryset):
+        for obj in queryset:
+            _challonge_auth()
+            challonge.tournaments.publish(tournament=obj.slugified_name)
+            challonge.tournaments.start(tournament=obj.slugified_name)
+            obj.is_active = False
+            obj.has_started = True
+            obj.save()
+
+    def add_to_challonge(self, request, queryset):
+        for obj in queryset:
+            tournament = obj
+            _challonge_auth()
+            tournament_name = "%s - %s" % (tournament.event.name, tournament.name)
+            challonge.tournaments.create(name=tournament_name, url=tournament.slugified_name, tournament_type=tournament.get_tournament_style())
 
 class TeamAdmin(admin.ModelAdmin):
-	pass
+    actions = ['add_to_challonge']
+
+    def add_to_challonge(self, request, queryset):
+        for obj in queryset:
+            team = obj
+            tournament = team.tournament
+            _challonge_auth()
+            challonge.participants.create(tournament=tournament.slugified_name, name=team)
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Tournament, TournamentAdmin)
