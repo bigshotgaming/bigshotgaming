611eedd74fac2b6ae0d7b1f503a99f933eade9dd
Thomas Wiedenbein <wiede1t@cmich.edu>
2012-10-06 23:56:34 -0400

    Changed schedule to sort by day.

--- schedule/views.py
+++ schedule/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from schedule.models import ScheduleItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.datastructures import SortedDict
 from events.models import Event
 
 
@@ -12,17 +13,18 @@ def index(request):
     except (TypeError, ObjectDoesNotExist):
         event = None
     try:
-        schedule = ScheduleItem.objects.filter(event=event)
+        schedule = ScheduleItem.objects.filter(event=event).order_by('start_time')
     except:
         schedule = None
-    # days = {}
-    #     for item in schedule:
-    #         if item.start_time.day is not in days:
-    #             days[start_time.day] = [item,]
-    #         else
-    #             days[start_time.day].append(item)
-    #     print days
+    # this MUST be replaced prior to Django 1.5, preferably with an OrderedDict
+    days = SortedDict()
+    for item in schedule:
+        if item.start_time.date() not in days:
+            days[item.start_time.date()] = [item]
+        else:
+            days[item.start_time.date()].append(item)
     return render(request, 'schedule/index.html', {
         'event': event,
         'schedule': schedule,
+        'days': days,
     })
