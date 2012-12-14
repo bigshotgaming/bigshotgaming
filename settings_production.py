2fbceb06687f9771202a033c5ff2d0509863d134
Thomas Wiedenbein <wiede1t@cmich.edu>
2012-12-14 01:48:57 -0500

    Added raven to requirements and INSTALLED_APPS for sentry usage

--- settings_production.py
+++ settings_production.py
@@ -148,6 +148,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'django.contrib.flatpages',
     'django.contrib.staticfiles',
+    'raven.contrib.django',
     'djcelery',
     'gunicorn',
     'registration',
