df3d810934bcfbf639b724f5b46bd4fcd7dfec4e
Tim Rowley <tim@ubuntu.(none)>
2013-01-09 21:51:12 -0800

    Version one of the new seatmap.

---
+++ static/seatmap/js/client_seatmap.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    seatmap_id = 1;
+
+	show_grid = false;
+	GRID_INC = 15;
+	preview_container = '#preview';
+    create_canvas('#seatmap-container'); // height and width are dependent on the parent element
+
+    init_canvas({
+		canvas_id : 'seatmap-canvas',
+		zoom_out_selector: '#zoom-out-button',
+		zoom_in_selector: '#zoom-in-button',
+		select_seat_selector: '#select-seat-button',
+		pan_selector: '#pan-button',
+	});
+    populate_canvas(seatmap_id);
+    $('#seatmap-message').hide();
+    $('#seatmap-toolbar').show();
+	$('#select-seat-button').click();
+	$('button').button();
+});
