60776dcfecb8382d690047c3b41dcc47de61dcb2
Thomas Wiedenbein <wiede1t@cmich.edu>
2013-02-22 03:01:35 -0500

    Added Django 1.3 hexdigest implementation temporarily

--- auth/backends.py
+++ auth/backends.py
@@ -3,6 +3,24 @@ from django.contrib.auth.backends import ModelBackend
 from django.utils.encoding import smart_str
 from django.utils.hashcompat import md5_constructor, sha_constructor
 
+def get_hexdigest(algorithm, salt, raw_password):
+    """
+    Returns a string of the hexdigest of the given plaintext password and salt
+    using the given algorithm ('md5', 'sha1' or 'crypt').
+    """
+    raw_password, salt = smart_str(raw_password), smart_str(salt)
+    if algorithm == 'crypt':
+        try:
+            import crypt
+        except ImportError:
+            raise ValueError('"crypt" password algorithm not supported in this environment')
+        return crypt.crypt(raw_password, salt)
+
+    if algorithm == 'md5':
+        return md5_constructor(salt + raw_password).hexdigest()
+    elif algorithm == 'sha1':
+        return sha_constructor(salt + raw_password).hexdigest()
+    raise ValueError("Got unknown password algorithm type in password.")
 
 def _new_hexdigest(algorithm, salt, raw_password):
     """
