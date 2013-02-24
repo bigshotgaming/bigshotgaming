72148121fa2f850c981137512658524dc9a0f4fc
Thomas Wiedenbein <wiede1t@cmich.edu>
2013-02-23 19:52:13 -0500

    Revamped Joomla hasher so that it actually functions properly and doesn't cause 500s

--- joomla_password_hasher.py
+++ joomla_password_hasher.py
@@ -1,13 +1,27 @@
 import hashlib
 from django.contrib.auth.hashers import BasePasswordHasher
+from django.utils.crypto import constant_time_compare
 
 class JoomlaPasswordHasher(BasePasswordHasher):
     algorithm = "joomla_md5"
 
     def encode(self, password, salt):
-        hash = hashlib.md5(force_bytes(password + salt)).hexdigest()
+        assert password
+        assert salt and '$' not in salt
+        hash = hashlib.md5(password + salt).hexdigest()
         return "%s$%s$%s" % (self.algorithm, salt, hash)
 
     def verify(self, password, encoded):
         algorithm, salt, hash = encoded.split('$', 2)
-        self.encode(password,)
+        assert algorithm == self.algorithm
+        encoded_2 = self.encode(password, salt)
+        return constant_time_compare(encoded, encoded_2)
+
+    def safe_summary(self, encoded):
+        algorithm, salt, hash = encoded.split('$', 2)
+        assert algorithm == self.algorithm
+        return SortedDict([
+            (_('algorithm'), algorithm),
+            (_('salt'), mask_hash(salt, show=2)),
+            (_('hash'), mask_hash(hash)),
+    ])
